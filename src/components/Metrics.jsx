d0498229776fb83f6093ee965bc1fad1c93d9995
忘忧北萱草 <wybxc@qq.com>
2022-05-30 13:27:30 +0800

    解决SSR失败的问题

--- src/components/Metrics.jsx
+++ src/components/Metrics.jsx
@@ -1,15 +1,21 @@
 import { Image, Shimmer } from 'react-shimmer'
 
 import React from 'react'
+import useIsBrowser from '@docusaurus/useIsBrowser'
 import useWindowDimensions from './useWindowDimensions'
 
-const Metrics = () => {
+const ResponsiveMetrics = () => {
   const { width } = useWindowDimensions()
   const src =
     width >= 768
       ? 'https://github.com/Wybxc/metrics/raw/main/github-metrics.svg'
       : 'https://github.com/Wybxc/metrics/raw/main/github-metrics-small.svg'
   return <Image src={src} fallback={<Shimmer width="100%" height={600} />} />
+}
+
+const Metrics = () => {
+  const isBrowser = useIsBrowser()
+  return isBrowser ? <ResponsiveMetrics /> : <Shimmer width="100%" height={600} />
 }
 
 export default Metrics
