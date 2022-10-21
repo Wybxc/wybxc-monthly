8275e6b62425eb4e6ae0c830557d506bebacf5bb
忘忧北萱草 <wybxc@qq.com>
2022-10-21 12:01:42 +0800

    fix: giscus 跨页面不刷新

--- src/theme/MDXContent/index.tsx
+++ src/theme/MDXContent/index.tsx
@@ -1,16 +1,27 @@
+import React, { useEffect } from 'react'
+
+import BrowserOnly from '@docusaurus/BrowserOnly'
 import Giscus from '@giscus/react'
 import MDXContent from '@theme-original/MDXContent'
-import React from 'react'
 import Tips from '@site/src/components/Tips'
 import { useColorMode } from '@docusaurus/theme-common'
 
-const MDXContentWrapper: React.FC = (props) => {
+const PageFooter = () => {
   const { colorMode } = useColorMode()
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const giscus = document.getElementById('giscus-comments') as any      
+      giscus.requestUpdate()
+    }, 1500)
+    return () => clearTimeout(timer)
+  }, [document.title])
+
   return (
     <>
-      <MDXContent {...props} />
       <br />
       <Giscus
+        id="giscus-comments"
         repo="Wybxc/wybxc-monthly"
         repoId="R_kgDOHaWsQg"
         category="Giscus 评论"
@@ -22,8 +33,18 @@ const MDXContentWrapper: React.FC = (props) => {
         theme={colorMode == 'dark' ? 'dark_protanopia' : 'light_protanopia'}
         lang="zh-CN"
         loading="eager"
+        key={document.title}
       />
       <Tips />
+    </>
+  )
+}
+
+const MDXContentWrapper: React.FC = (props) => {
+  return (
+    <>
+      <MDXContent {...props} />
+      <BrowserOnly fallback={<div>Loading...</div>}>{() => <PageFooter />}</BrowserOnly>
     </>
   )
 }
